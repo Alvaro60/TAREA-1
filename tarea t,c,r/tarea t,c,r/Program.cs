451c1932dbbe5669365c8bc31b9696d57f21dd50
Alvaro60 <vergildante54@gmail.com>
2021-04-01 11:32:46 -0300

    initial commit

---
+++ tarea t,c,r/tarea t,c,r/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_t_c_r
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
